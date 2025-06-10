9ae3597dac2a4dcd2ccf23eab4e6e72101b6e410
aldisrauf <aldirauf@gmail.com>
2025-06-10 13:09:12 +0700

    edit lokasi shiny

--- R/run_simple_app.R
+++ R/run_simple_app.R
@@ -1,10 +1,10 @@
 #' Jalankan Aplikasi Shiny Sederhana
 #'
-#' Menjalankan aplikasi Shiny contoh sederhana yang ada dalam package.
+#' Menjalankan contoh aplikasi Shiny dari package basictools.
 #'
 #' @export
 run_simple_app <- function() {
-  app_dir <- system.file("shiny_examples", package = "basictools")
+  app_dir <- system.file("shiny/simple_app", package = "basictools")
   if (app_dir == "") {
     stop("Folder aplikasi Shiny tidak ditemukan di dalam package.", call. = FALSE)
   }
