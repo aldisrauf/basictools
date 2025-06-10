c6c9c64757ef12363295e9610ff9aea489a62b96
aldisrauf <aldirauf@gmail.com>
2025-06-10 13:13:02 +0700

    debug shiny

--- R/run_simple_app.R
+++ R/run_simple_app.R
@@ -5,6 +5,14 @@
 #' @export
 run_simple_app <- function() {
   app_dir <- system.file("shiny/simple_app", package = "basictools")
+  validExamples <- list.files(app_dir)
+
+  validExamplesMsg <-
+    paste0(
+      "Valid examples are: '",
+      paste(validExamples, collapse = "', '"),
+      "'")
+
   if (app_dir == "") {
     stop("Folder aplikasi Shiny tidak ditemukan di dalam package.", call. = FALSE)
   }
